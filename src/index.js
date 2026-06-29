22035965c7e01439210b752b899ed540f8e61136
Godzy_Me <Pradnesh.khunte@gmail.com>
2026-06-29 18:14:50 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,40 +1,26 @@
 // required('dotenv').config({path:'./env'})
 
+
 import dotenv from "dotenv"
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
 import dns from "node:dns/promises";
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
+
 dotenv.config({
     path : '/.env'
 })
 
-
 connectDB()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("MongoDB connection failed !!!", error)
+})
 
 /*
 (async ()=>{
